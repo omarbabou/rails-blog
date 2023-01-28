59ad743baa171132c838dbac0fe9813ec624df0e
omarbabou <obabou50@gmail.com>
2023-01-28 12:07:19 +0200

    Add routes and controllers to the project

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  root "user#index"
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  root "users#index"
   resources :users, only: [:index, :show] do
     resources :posts, only: [:index, :show]
   end
