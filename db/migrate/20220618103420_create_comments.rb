01991a47404b0d517d2fad29226779bda6008055
omarbabou <obabou50@gmail.com>
2023-02-04 14:07:55 +0200

    Add spec folders to the project

--- db/migrate/20220618103420_create_comments.rb
+++ db/migrate/20220618103420_create_comments.rb
@@ -2,7 +2,7 @@ class CreateComments < ActiveRecord::Migration[7.0]
     def change
       create_table :comments do |t|
         t.references :author, foreign_key: {to_table: 'users'}     
-        t.text :text
+        t.text :text, null:false
         t.belongs_to :posts, null: false, foreign_key: true
   
         t.timestamps
