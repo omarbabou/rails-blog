4922e7c9b691d2a5c96bd04415a796a699c1b2cc
omarbabou <obabou50@gmail.com>
2023-02-11 10:31:11 +0200

    Add sessions to the project

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,35 @@
 class User < ApplicationRecord
-  has_many :posts, foreign_key: 'author_id'
-  has_many :comments, foreign_key: 'author_id'
-  has_many :likes, foreign_key: 'author_id'
+  attr_writer :login
+
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+  has_many :posts, foreign_key: 'author_id', dependent: :destroy
+  has_many :likes, foreign_key: 'author_id', dependent: :destroy
+  has_many :comments, foreign_key: 'author_id', dependent: :destroy
+  validates :name, presence: true
+  validates_format_of :name, with: /^[a-zA-Z0-9_.]*$/, multiline: true
 
   def recent_posts
-    posts.order(created_at: :desc).limit(3)
+    posts.order('created_at DESC').limit(3)
+  end
+
+  def login
+    @login || name || email
+  end
+
+  def self.find_for_database_authentication(warden_conditions)
+    conditions = warden_conditions.dup
+    if (login = conditions.delete(:login))
+      where(conditions.to_h).where(['name = :value OR email = :value', { value: login }]).first
+    elsif conditions.key?(:name) || conditions.key?(:email)
+      where(conditions.to_h).first
+    end
+  end
+
+  def admin?
+    role == 'admin'
   end
 end
+
