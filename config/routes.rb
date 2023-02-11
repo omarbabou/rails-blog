a3790a593ba089d457b1d4d8c0cdf772adbe53ae
omarbabou <obabou50@gmail.com>
2023-02-11 13:08:21 +0200

    Configure routes

--- config/routes.rb
+++ config/routes.rb
@@ -11,14 +11,12 @@ Rails.application.routes.draw do
   end
   
   resources :users, only: [:index, :show] do
-    resources :posts, only: [:index, :show, :destroy] do
-      resources :comments, only: [:create, :destroy]
+    resources :posts, only: [:index, :new,:create,:show] do
+      resources :comments, only: [:new, :create]
       resources :likes, only: [:create]
   end
   end
 
-  get "/posts/new", to: "posts#new"
-  post "/posts/create", to: "posts#create"
-  post "/users/:user_id/posts/:id/like", to: "likes#create"
-  post "/users/:user_id/posts/:id/create_comment", to: "comments#create"
-end 
+  # Defines the root path route ("/")
+  # root "articles#index"
+end
