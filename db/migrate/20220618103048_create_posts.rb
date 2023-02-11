696284aba7c7de203921a643ac626aff6ac74e32
omarbabou <obabou50@gmail.com>
2023-02-11 19:32:57 +0200

    Add changes

--- db/migrate/20220618103048_create_posts.rb
+++ db/migrate/20220618103048_create_posts.rb
@@ -7,15 +7,6 @@ class CreatePosts < ActiveRecord::Migration[7.0]
       t.timestamps
       t.bigint :comments_counter, null:false, default: 0
       t.bigint :likes_counter, null:false, default: 0
-    def change
-      create_table :posts do |t|
-        t.string :title, null:false, :limit => 32
-        t.text :text, null:false
-  
-        t.timestamps
-        t.bigint :comments_counter, null:false, default: 0
-        t.bigint :likes_counter, null:false, default: 
-      end
     end
   end
 end
