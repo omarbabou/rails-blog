ad38f285d7e4ae8d240bf9901e97af6a3be5ef0c
omarbabou <obabou50@gmail.com>
2023-02-11 12:47:35 +0200

    Change application controller and add configure_permitted_parameters

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -16,8 +16,10 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: %i[name photo bio email])
+    devise_parameter_sanitizer.permit(:sign_up, keys: %i[name photo bio])
     devise_parameter_sanitizer.permit(:account_update, keys: %i[name photo bio])
-    devise_parameter_sanitizer.permit :sign_in, keys: %i[login password]
+    devise_parameter_sanitizer.permit(:sign_in) do |user_params|
+      user_params.permit(:username, :email)
+    end
   end
 end
