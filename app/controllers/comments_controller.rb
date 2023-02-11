7ff38c04d6ef4523264e7c18affb18504e5d83ea
omarbabou <obabou50@gmail.com>
2023-02-11 10:01:46 +0200

    Add authenticate_user to the comments_controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,28 +1,47 @@
 class CommentsController < ApplicationController
-  def new
-    respond_to do |format|
-      format.html { render :new, locals: { comment: Comment.new } }
+    before_action :authenticate_user!
+  
+    def create
+      @comment = current_user.comments.new(comment_params)
+      @comment.update_comments_counter
+      respond_to do |format|
+        flash[:notice] = if @comment.save
+                           'Comment created Successfully'
+                         else
+                           'something went wrong'
+                         end
+        format.html { redirect_to request.path }
+      end
+    end
+  
+    def destroy
+      @comment = Comment.find(params[:id])
+      @post = @comment.post
+    end
+  
+    def comment_params
+      params.require(:comment).permit(:author_id, :post_id, :text)
     end
   end
-
-  def create
-    user = current_user
-    post = current_post
-    comment = Comment.new(comment_params)
-    comment.author = user
-    comment.post = post
-    if comment.save
-      flash[:success] = 'Comment saved successfully'
-      redirect_to user_post_url(id: post.id)
-    else
-      flash[:error] = 'Error: Comment could not be saved'
-      redirect_to new_user_post_comment_url
+    
+      def create
+        user = current_user
+        post = current_post
+        comment = Comment.new(comment_params)
+        comment.author = user
+        comment.post = post
+        if comment.save
+          flash[:success] = 'Comments were saved successfully'
+          redirect_to user_post_url(id: post.id)
+        else
+          flash[:error] = 'Error: Could not save comments'
+          redirect_to new_user_post_comment_url
+        end
+      end
+    
+      private
+    
+      def comment_params
+        params.require(:comment).permit(:text)
+      end
     end
-  end
-
-  private
-
-  def comment_params
-    params.require(:comment).permit(:text)
-  end
-end
