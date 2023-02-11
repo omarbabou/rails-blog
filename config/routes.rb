594e4281dbeeecb1298c051daf8a6f5d8029e31b
omarbabou <obabou50@gmail.com>
2023-02-11 10:35:19 +0200

    Update the integration for spec files

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,25 @@
 Rails.application.routes.draw do
-   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-   root "users#index"
-  resources :users, only: [:index, :show] do
-    resources :posts, only: [:index, :show]
+  devise_for :users
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  devise_scope :user do
+    authenticated :user do
+      root :to => "users#index", as: :authenticated_root
+    end
+    unauthenticated :user do
+      root :to => "devise/sessions#new", as: :unauthenticated_root
+    end
   end
-   # Defines the root path route ("/")
-  # root "articles#index"
-end  
+
+  resources :users, only: [:index, :show] do
+    resources :posts, only: [:index, :show, :destroy] do
+      resources :comments, only: [:create, :destroy]
+    resources :posts, only: [:index, :new,:create,:show] do
+      resources :comments, only: [:new, :create]
+      resources :likes, only: [:create]
+  end
+  end
+  get "/posts/new", to: "posts#new"
+  post "/posts/create", to: "posts#create"
+  post "/users/:user_id/posts/:id/like", to: "likes#create"
+  post "/users/:user_id/posts/:id/create_comment", to: "comments#create"
+end
